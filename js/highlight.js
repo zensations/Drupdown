0f8aa39abde072d4d5e3b4d28cb525b20eb07c08
philipp <pm@zensations.at>
2012-08-27 09:57:53 +0200

    fixed broken syntax highlighter

--- js/highlight.js
+++ js/highlight.js
@@ -21,124 +21,111 @@ define('drupdown/highlight', ['require', 'exports', 'module' , 'ace/lib/oop', 'a
         // regexp must not have capturing parentheses
         // regexps are ordered -> the first match is used
 
-        this.$rules = {
-            "start" : [ {
-                token : "empty_line",
-                regex : '^$'
-            }, { // code span `
-                token : "support.function",
-                regex : "(`+)([^\\r]*?[^`])(\\1)"
-            }, { // code block
-                token : "support.function",
-                regex : "^[ ]{4}.+"
-            }, { // h1
-                token: "markup.heading.1",
-                regex: "^=+(?=\\s*$)"
-            }, { // h2
-                token: "markup.heading.1",
-                regex: "^\\-+(?=\\s*$)"
-            }, { // header
-                token : function(value) {
-                    return "markup.heading." + value.length;
-                },
-                regex : "^#{1,6}.*$"
-            },
-            github_embed("javascript", "js-"),
-            github_embed("xml", "xml-"),
-            github_embed("html", "html-"),
-            github_embed("css", "css-"),
-            { // Github style block
-                token : "support.function",
-                regex : "^```[a-zA-Z]+\\s*$",
-                next  : "githubblock"
-            }, { // block quote
-                token : "string",
-                regex : "^[<>!][ ].+$",
-                next  : "blockquote"
-            }, { // reference
-                token : ["text", "string", "text", "support.function", "string", "text"],
-                regex : "^([ ]{0,3}\\[)([^\\]]+)(\\]:\\s*)([^ ]+)(\\s*(?:[\"][^\"]+[\"])?\\s*)$"
-            }, { // link by reference
-                token : ["text", "string", "text", "support.function", "text"],
-                regex : "(\\[)((?:[[^\\]]*\\]|[^\\[\\]])*)(\\][ ]?(?:\\n[ ]*)?\\[)(.*?)(\\])"
-            }, { // simple reference
-                token : ["text", "string", "text"],
-                regex : "(\\[)((?:[[^\\]]*\\]|[^\\[\\]])*)(\\])"
-            }, { // link by url
-                token : ["text", "string", "text"],
-                regex : "(\\[)"+
-                    "(\\[[^\\]]*\\]|[^\\[\\]]*)"+
-                    "(\\]\\([ \\t]*)"+
-                    "(<?(?:(?:[^\\(]*?\\([^\\)]*?\\)\\S*?)|(?:.*?))>?)"+
-                    "((?:[ \t]*\"(?:.*?)\"[ \\t]*)?)"+
-                    "(\\))"
-            }, { // embed
-                token : ["markup.list", "markup.list", "string", "markup.list", "support.function", "string", "markup.list"],
-                regex : "([<>!])" +
-                    "(\\[)"+
-                    "(\\[[^\\]]*\\]|[^\\[\\]]*)"+
-                    "(\\]\\([ \\t]*)"+
-                    "(<?(?:(?:[^\\(]*?\\([^\\)]*?\\)\\S*?)|(?:.*?))>?)"+
-                    "((?:[ \t]*\"(?:.*?)\"[ \\t]*)?)"+
-                    "(\\))"
-            }, { // HR *
-                token : "constant",
-                regex : "^[ ]{0,2}(?:[ ]?\\*[ ]?){3,}\\s*$"
-            }, { // HR -
-                token : "constant",
-                regex : "^[ ]{0,2}(?:[ ]?\\-[ ]?){3,}\\s*$"
-            }, { // HR _
-                token : "constant",
-                regex : "^[ ]{0,2}(?:[ ]?\\_[ ]?){3,}\\s*$"
-            }, { // list
-                token : "markup.list",
-                regex : "^\\s{0,3}(?:[*+-]|\\d+\\.)\\s+",
-                next  : "listblock"
-            }, { // strong ** __
-                token : "string",
-                regex : "([*]{2}|[_]{2}(?=\\S))([^\\r]*?\\S[*_]*)(\\1)"
-            }, { // emphasis * _
-                token : "string",
-                regex : "([*]|[_](?=\\S))([^\\r]*?\\S[*_]*)(\\1)"
-            }, { // 
-                token : ["text", "url", "text"],
-                regex : "(<)("+
-                    "(?:https?|ftp|dict):[^'\">\\s]+"+
-                    "|"+
-                    "(?:mailto:)?[-.\\w]+\\@[-a-z0-9]+(?:\\.[-a-z0-9]+)*\\.[a-z]+"+
-                    ")(>)"
-            }, {
-                token : "text",
-                regex : "[^\\*_%$`\\[#<>]+"
-            } ],
+      this.$rules = {
+        "start" : [ {
+          token : "empty_line",
+          regex : '^$'
+        }, { // code span `
+          token : ["support.function", "support.function", "support.function"],
+          regex : "(`+)([^\\r]*?[^`])(\\1)"
+        }, { // code block
+          token : "support.function",
+          regex : "^[ ]{4}.+"
+        }, { // h1
+          token: "markup.heading.1",
+          regex: "^=+(?=\\s*$)"
+        }, { // h2
+          token: "markup.heading.1",
+          regex: "^\\-+(?=\\s*$)"
+        }, { // header
+          token : function(value) {
+            return "markup.heading." + value.length;
+          },
+          regex : "^#{1,6}"
+        }, github_embed("(?:javascript|js)", "js-"),
+          github_embed("xml", "xml-"),
+          github_embed("html", "html-"),
+          github_embed("css", "css-"),
+          { // Github style block
+            token : "support.function",
+            regex : "^```[a-zA-Z]+\\s*$",
+            next  : "githubblock"
+          }, { // block quote
+            token : "string",
+            regex : "^>[ ].+$",
+            next  : "blockquote"
+          }, { // reference
+            token : ["text", "constant", "text", "url", "string", "text"],
+            regex : "^([ ]{0,3}\\[)([^\\]]+)(\\]:\\s*)([^ ]+)(\\s*(?:[\"][^\"]+[\"])?(\\s*))$"
+          }, { // link by reference
+            token : ["text", "string", "text", "constant", "text"],
+            regex : "(\\[)((?:[[^\\]]*\\]|[^\\[\\]])*)(\\][ ]?(?:\\n[ ]*)?\\[)(.*?)(\\])"
+          }, { // link by url
+            token : ["text", "string", "text", "markup.underline", "string", "text"],
+            regex : "(\\[)"+
+                "(\\[[^\\]]*\\]|[^\\[\\]]*)"+
+                "(\\]\\([ \\t]*)"+
+                "(<?(?:(?:[^\\(]*?\\([^\\)]*?\\)\\S*?)|(?:.*?))>?)"+
+                "((?:[ \t]*\"(?:.*?)\"[ \\t]*)?)"+
+                "(\\))"
+          }, { // HR *
+            token : "constant",
+            regex : "^[ ]{0,2}(?:[ ]?\\*[ ]?){3,}\\s*$"
+          }, { // HR -
+            token : "constant",
+            regex : "^[ ]{0,2}(?:[ ]?\\-[ ]?){3,}\\s*$"
+          }, { // HR _
+            token : "constant",
+            regex : "^[ ]{0,2}(?:[ ]?\\_[ ]?){3,}\\s*$"
+          }, { // list
+            token : "markup.list",
+            regex : "^\\s{0,3}(?:[*+-]|\\d+\\.)\\s+",
+            next  : "listblock"
+          }, { // strong ** __
+            token : ["string", "string", "string"],
+            regex : "([*]{2}|[_]{2}(?=\\S))([^\\r]*?\\S[*_]*)(\\1)"
+          }, { // emphasis * _
+            token : ["string", "string", "string"],
+            regex : "([*]|[_](?=\\S))([^\\r]*?\\S[*_]*)(\\1)"
+          }, { //
+            token : ["text", "url", "text"],
+            regex : "(<)("+
+                "(?:https?|ftp|dict):[^'\">\\s]+"+
+                "|"+
+                "(?:mailto:)?[-.\\w]+\\@[-a-z0-9]+(?:\\.[-a-z0-9]+)*\\.[a-z]+"+
+                ")(>)"
+          }, {
+            token : "text",
+            regex : "[^\\*_%$`\\[#<>]+"
+          } ],
 
-            "listblock" : [ { // Lists only escape on completely blank lines.
-                token : "empty_line",
-                regex : "^$",
-                next  : "start"
-            }, {
-                token : "markup.list",
-                regex : ".+"
-            } ],
+        "listblock" : [ { // Lists only escape on completely blank lines.
+          token : "empty_line",
+          regex : "^$",
+          next  : "start"
+        }, {
+          token : "markup.list",
+          regex : ".+"
+        } ],
 
-            "blockquote" : [ { // BLockquotes only escape on blank lines.
-                token : "empty_line",
-                regex : "^\\s*$",
-                next  : "start"
-            }, {
-                token : "string",
-                regex : ".+"
-            } ],
+        "blockquote" : [ { // BLockquotes only escape on blank lines.
+          token : "empty_line",
+          regex : "^\\s*$",
+          next  : "start"
+        }, {
+          token : "string",
+          regex : ".+"
+        } ],
 
-            "githubblock" : [ {
-                token : "support.function",
-                regex : "^```",
-                next  : "start"
-            }, {
-                token : "support.function",
-                regex : ".+"
-            } ]
-        };
+        "githubblock" : [ {
+          token : "support.function",
+          regex : "^```",
+          next  : "start"
+        }, {
+          token : "support.function",
+          regex : ".+"
+        } ]
+      };
 
         this.embedRules(JavaScriptHighlightRules, "js-", [{
             token : "support.function",
